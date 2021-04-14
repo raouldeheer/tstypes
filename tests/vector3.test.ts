a72fbe8412020502aefc91b13487b5130b2b66fd
Raoul de Heer <raouldeheer@users.noreply.github.com>
2021-04-14 23:03:02 +0200

    🩹Fixed matrices

--- tests/vector3.test.ts
+++ tests/vector3.test.ts
@@ -6,7 +6,7 @@ describe("Test Vector2", () => {
         const m1 = new Vector3(2, 4, 7);
         const m2 = m1.T;
         const m3 = m1["*"](m2);
-        const correct = new Matrix([4, 8, 14], [8, 16, 28], [14, 28, 49]);
+        const correct = new Matrix([[4, 8, 14], [8, 16, 28], [14, 28, 49]]);
         expect(m3).toStrictEqual(correct);
     });
     it("Should add vector2", () => {
@@ -15,7 +15,6 @@ describe("Test Vector2", () => {
         const m3 = m1["+"](m2);
         const correct = new Vector3(5, -3, 14);
         expect(m3).toStrictEqual(correct);
-        expect(m1).toStrictEqual(correct);
     });
     test("Should deduct vector2", () => {
         const m1 = new Vector3(2, 4, 7);
@@ -23,6 +22,5 @@ describe("Test Vector2", () => {
         const m3 = m1["-"](m2);
         const correct = new Vector3(-6, 1, 0);
         expect(m3).toStrictEqual(correct);
-        expect(m1).toStrictEqual(correct);
     });
 });
--- ts/index.ts
+++ ts/index.ts
@@ -21,9 +21,11 @@ export declare type callback<T, U = unknown, V = unknown, W = unknown> = (
 
 export class Matrix {
     rows: number[][];
-    protected type: string;
-    constructor(..._rows: number[][]) {
-        this.type = "Matrix";
+    factor: number;
+    protected readonly type: MatrixType;
+    constructor(_rows: number[][], _factor?: number, _type?: MatrixType) {
+        this.factor = _factor ?? 1;
+        this.type = _type ?? MatrixType.Matrix;
         if (_rows.length <= 0) throw new Error("Invalid matrix");
         const columCount = _rows[0].length;
         if (!_rows.every(v => columCount === v.length)) throw new Error("Invalid matrix");
@@ -31,7 +33,7 @@ export class Matrix {
     }
 
     public get colums(): number[][] {
-        let colums = [];
+        const colums = [];
         for (let i = 0; i < this.columCount; i++) {
             colums.push(this.rows.reduce((prev: number[], curr: number[]) => [...prev, curr[i]], []));
         }
@@ -44,48 +46,77 @@ export class Matrix {
         return this.rows[0].length;
     }
     public get T(): Matrix {
-        return new Matrix(...this.colums);
+        return new Matrix(this.colums);
+    }
+    public get clone(): Matrix {
+        return new Matrix(this.rows);
     }
 
-    '*'(m: Matrix) {
+    protected calcRows(m: Matrix, opDir: OpDir): number[][] {
+        return this.rows.map((v, i) => v.map((v2, i2) =>
+            opDir === OpDir.Plus ? v2 + m.rows[i][i2] : v2 - m.rows[i][i2]));
+    }
+
+    '*'(m: Matrix): Matrix {
         if (m.rowCount !== this.columCount) throw new Error("Invalid oparation");
-        let newRows = [];
+        const newRows = [];
         const mColums = m.colums;
         for (let i = 0; i < this.rowCount; i++) {
-            let newRow = [];
+            const newRow = [];
             for (let j = 0; j < m.columCount; j++) {
                 newRow.push(this.rows[i].reduce((prev: number, curr: number, i2: number) => prev + (mColums[j][i2] * curr), 0));
             }
             newRows.push(newRow);
         }
-        if (this.type === "Matrix") {
-            this.rows = newRows;
-            return this as Matrix;
-        } else {
-            return new Matrix(...newRows);
-        }
+        return new Matrix(newRows);
     }
-    '+'(m: Matrix) {
+    '+'(m: Matrix): Matrix {
         if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
-        this.rows = this.rows.map((v: number[], i: number) => v.map((v2: number, i2: number) => v2 + m.rows[i][i2]));
-        return this;
+        return new Matrix(this.calcRows(m, OpDir.Plus));
     }
-    '-'(m: Matrix) {
+    '-'(m: Matrix): Matrix {
         if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
-        this.rows = this.rows.map((v: number[], i: number) => v.map((v2: number, i2: number) => v2 - m.rows[i][i2]));
-        return this;
+        return new Matrix(this.calcRows(m, OpDir.Min));
     }
 }
 
+enum OpDir {
+    Plus = 1,
+    Min = -1,
+}
+enum MatrixType {
+    Matrix,
+    Vector2,
+    Vector3,
+}
+
 export class Vector2 extends Matrix {
-    constructor(x: number, y: number) {
-        super([x], [y]);
-        this.type = "Vector2";
+    constructor(x: number, y: number, factor?: number) {
+        super([[x], [y]], factor, MatrixType.Vector2);
+    }
+    '+'(m: Matrix): Vector2 {
+        if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
+        const newRows = this.calcRows(m, OpDir.Plus);
+        return new Vector2(newRows[0][0], newRows[1][0]);
+    }
+    '-'(m: Matrix): Vector2 {
+        if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
+        const newRows = this.calcRows(m, OpDir.Min);
+        return new Vector2(newRows[0][0], newRows[1][0]);
     }
 }
 export class Vector3 extends Matrix {
-    constructor(x: number, y: number, z: number) {
-        super([x], [y], [z]);
-        this.type = "Vector3";
+    constructor(x: number, y: number, z: number, factor?: number) {
+        super([[x], [y], [z]], factor, MatrixType.Vector3);
+    }
+    '+'(m: Matrix): Vector2 {
+        if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
+        const newRows = this.calcRows(m, OpDir.Plus);
+        return new Vector3(newRows[0][0], newRows[1][0], newRows[2][0]);
+    }
+    '-'(m: Matrix): Vector2 {
+        if (m.rowCount !== this.rowCount || m.columCount !== this.columCount) throw new Error("Invalid oparation");
+        const newRows = this.calcRows(m, OpDir.Min);
+        return new Vector3(newRows[0][0], newRows[1][0], newRows[2][0]);
     }
 }
